8206b52231f9ad888862c70bf438c69f3fa71c99
talesrodovalhozup <tales.rodovalho@zup.com.br>
2020-09-30 17:00:12 -0300

    Atualizações

--- cypress/helpers/testA11y.js
+++ cypress/helpers/testA11y.js
@@ -1,19 +1,21 @@
-import 'cypress-axe';
+import "cypress-axe";
 
 const A11Y_OPTIONS = {
   runOnly: {
-    type: 'tag',
+    type: "tag",
     values: [
-      'section508',
-      'wcag2a',
-      'wcag2aa',
-      'wcag21a',
-      'wcag21aa',
-      'wcag143',
-      'wcag242',
-      'cat',
-    ],
-  },
+      "section508",
+      "section508.22.i",
+      "wcag2a",
+      "wcag2aa",
+      "wcag21a",
+      "wcag21aa",
+      "wcag143",
+      "wcag242",
+      "wcag311",
+      "cat"
+    ]
+  }
 };
 
 export default function testA11y() {
--- cypress/tests/e2e/testFlow.spec.js
+++ cypress/tests/e2e/testFlow.spec.js
@@ -1,4 +1,5 @@
 import CREDICARD_BLACK from "../../constants/CREDICARD_BLACK";
+import testA11y from "../../helpers/testA11y";
 
 const {
   CUSTOMER: { ID }
@@ -6,6 +7,7 @@ const {
 
 describe("Credicard ZERO Indicated Page Integration Tests", () => {
   it("should go through the card acquisition flow", () => {
-    cy.visit(`/?id=${ID}#/`);
+    cy.visit(`https://www.zup.com.br/`);
+    testA11y();
   });
 });
