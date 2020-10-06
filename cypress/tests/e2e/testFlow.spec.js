9c3b2297d6d3288bdf066412da6bd3ea333d3fa0
talesrodovalhozup <tales.rodovalho@zup.com.br>
2020-10-06 15:27:36 -0300

    retirando arquivos mortos

--- cypress/tests/e2e/testFlow.spec.js
+++ cypress/tests/e2e/testFlow.spec.js
@@ -1,12 +1,7 @@
-import CREDICARD_BLACK from "../../constants/CREDICARD_BLACK";
 import testA11y from "../../helpers/testA11y";
 
-const {
-  CUSTOMER: { ID }
-} = CREDICARD_BLACK;
-
-describe("Credicard ZERO Indicated Page Integration Tests", () => {
-  it("should go through the card acquisition flow", () => {
+describe("Zup Page accessibility Tests", () => {
+  it("should go through the page flow validating accessibility", () => {
     cy.visit(`https://www.zup.com.br/`);
     testA11y();
   });
